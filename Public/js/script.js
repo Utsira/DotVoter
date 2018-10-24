43f12f4bb3b86bf19040f610fb7203c1c54a7dab
Oliver Dew <oliver.dew@theappbusiness.com>
2018-10-24 13:58:01 +0100

    Card editing
    Msnry doesn’t update properly

--- Public/js/script.js
+++ Public/js/script.js
@@ -14,51 +14,69 @@ socket.addEventListener("message", ev => {
     reader.addEventListener("loadend", (ev) => {
         const text = ev.srcElement.result;
         const json = JSON.parse(text);
-        const grid = document.querySelector('.grid');
 
-       const cardElements = createCard(json);
-       const fragment = document.createDocumentFragment();
-       
-       cardElements.forEach(elem => {
-           fragment.appendChild(elem);
-       })
-       grid.appendChild(fragment);
-       msnry.appended(cardElements);
+        switch (json.action) {
+            case "new":
+                addNewCards(json);
+                break;
+            case "downVote":
+            case "edit":
+            case "upVote":
+                updateCards(json);
+                break;
+        }
+
         console.log(json);
     });
     reader.readAsText(ev.data);
 })
 
-function createCard(json) {
-    
-    return json.cards.map((card) => {
-        const elem = document.createElement('div');
-        elem.className = "grid-item";
-        elem.style = `height:${120 + card.voteCount * 20}px;`;
-        elem.id = card.id;
-        elem.innerHTML = `<h3 class="message">${card.message}</h3>`;
-        return elem;
+function updateCards(json) {
+    json.cards.forEach(card => {
+        const elem = document.getElementById(card.id);
+        const newElement = getCardElement(card);
+        elem.replaceWith(newElement);
+        msnry.layout();
     });
+}
+
+function addNewCards(json) {
+    const grid = document.querySelector('.grid');
+    const cardElements = json.cards.map(card => {
+        return getCardElement(card);
+    });
+    const fragment = document.createDocumentFragment();
+    cardElements.forEach(elem => {
+        fragment.appendChild(elem);
+    })
+    grid.appendChild(fragment);
+    msnry.appended(cardElements);
+}
+
+function getCardElement(card) {
+    const elem = document.createElement('div');
+    elem.className = "grid-item";
+    elem.style = `height:${120 + card.voteCount * 20}px;`;
+    elem.id = card.id;
+    elem.innerHTML = `<h3 class="message">${card.message}</h3>`;
+    return elem;
 }
 
 function setupMasonry() {
     const grid = document.querySelector('.grid');
     grid.addEventListener("click", (ev) => {
-        if ( !matchesSelector( ev.target, '.grid-item' ) ) {
+        if (!matchesSelector(ev.target, '.grid-item')) {
             return;
-          }
-
-          const payload = {
-              action: "upVote",
-                cards: [
-                    {voteCount: 0, id: ev.srcElement.id, message: "", category: ""}
-                ]
-          };
-          socket.send(JSON.stringify(payload));
-          
+        }
+        const payload = {
+            action: "upVote",
+            cards: [
+                { voteCount: 0, id: ev.srcElement.id, message: "", category: "" }
+            ]
+        };
+        socket.send(JSON.stringify(payload));
     });
     return new Masonry(grid, {
-        // options
         itemSelector: '.grid-item',
         columnWidth: 168
     });
